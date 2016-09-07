d39010059b52cd967ec043403f7e8ff674a4374d
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-07 13:43:36 -0500

    strings and arrays

---
+++ app.js
@@ -0,0 +1,13 @@
+var name = "John Doe";
+var city = "Chicago";
+
+console.log("Hello " + name);
+console.log("You live in " + city);
+
+var cars = ["Toyota Camry", "Honda Civic", "Toyota Corolla", "Nissan Altima",
+            "Honda Accord", "Ford Fusion", "Nissan Sentra", "Hyundai Sonata",
+            "Chevrolet Malibu", "Chevrolet Cruze"];
+
+console.log(cars.length);
+var carSort = cars.sort();
+console.log(carSort);
