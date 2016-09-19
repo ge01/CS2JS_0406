2a6c46203be25225cbbaddb32cf9dc6dff33d156
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-19 15:46:59 -0500

    working function

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 // Event listener that fires when a user clicks a button.
-document.getElementById("luluBtn").addEventListener("click",
-  document.getElementById('outLulu').innerHTML = "Hawaii-Aleutian"
-);
+document.getElementById("luluBtn").addEventListener("click", function(){
+  computeCity("Honolulu");
+});
 
-document.getElementById("FranBtn").addEventListener("click",
-  document.getElementById('outFran').innerHTML = "Pacific"
-);
+document.getElementById("ciscoBtn").addEventListener("click", function(){
+  computeCity("San Francisco");
+});
+
+document.getElementById("denBtn").addEventListener("click", function(){
+  computeCity("Denver");
+});
+
+document.getElementById("minnBtn").addEventListener("click", function(){
+  computeCity("Minneapolis");
+});
+
+document.getElementById("yorkBtn").addEventListener("click", function(){
+  computeCity("New York");
+});
+
+function computeCity(city) {
+  // Determine the time zone
+  switch (city)
+  {
+    case "Honolulu":
+      document.getElementById('outCity').innerHTML = "Hawaii-Aleutian";
+      break;
+    case "San Francisco":
+      document.getElementById('outCity').innerHTML = "Pacific";
+      break;
+    case "Denver":
+      document.getElementById('outCity').innerHTML = "Mountain";
+      break;
+    case "Minneapolis":
+      document.getElementById('outCity').innerHTML = "Central";
+      break;
+    case "New York":
+      document.getElementById('outCity').innerHTML = "Eastern";
+      break;
+  }
+}
